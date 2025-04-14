55db22d3e36ac9b932d70dc579a27dc1d0d59840
Andrei-Krautsou <AndreiKrautsouGit@yandex.ru>
2025-04-14 06:13:31 +0300

    delete: search

--- src/pages/homepage/home-page.tsx
+++ src/pages/homepage/home-page.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom'
 
-import { ChangeEvent, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useExpand, useWebApp } from '@vkruglikov/react-telegram-web-app'
 
 import bgStart from '@/assets/imgs/bg_home_page.webp'
@@ -8,25 +8,13 @@ import bgStart from '@/assets/imgs/bg_home_page.webp'
 import bird from '@/assets/imgs/bird.svg'
 import { useGetLeaders } from '@/utils/api/hooks/user/use-get-leaders'
 import TableRang from './ui/table-rang/table-rang'
-import { Leader } from '@/types/api'
-import { Input } from '@/components/ui/input'
 
 export const HomePage = () => {
   const [isMobile, setIsMobile] = useState<boolean | null>(null)
   const WebApp = useWebApp()
   const [_isExpanded, expand] = useExpand()
 
-  const [allLeaders, setAllLeaders] = useState<Leader[]>([])
-  const [filteredLeaders, setFilteredLeaders] = useState<Leader[]>([])
-
   const { data: leaders } = useGetLeaders()
-
-  useEffect(() => {
-    if (leaders) {
-      setAllLeaders(leaders)
-      setFilteredLeaders(leaders) // Устанавливаем начальное состояние для отфильтрованных лидеров
-    }
-  }, [leaders])
 
   useEffect(() => {
     expand()
@@ -42,15 +30,6 @@ export const HomePage = () => {
       setIsMobile(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))
     }
   }, [])
-
-  const handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const value = event.currentTarget.value.toLocaleLowerCase()
-
-    const filtered = allLeaders.filter((item) => item.username.toLocaleLowerCase().includes(value))
-
-    setFilteredLeaders(filtered)
-    console.log('Filtered leaders:', filtered)
-  }
 
   return (
     <>
@@ -96,40 +75,23 @@ export const HomePage = () => {
           >
             Звездный рейс с <Link to={'https://www.kp.ru/'}>KP.RU </Link>
           </h1>
-          <div className='mb-5'>
-            <label
-              htmlFor='leaderSearch'
-              className='mb-2 block'
-              style={{
-                color: 'white',
-                fontFamily: 'futurespore_cyrillicregular',
-              }}
-            >
-              Поиск по имени
-            </label>
-            <Input id='leaderSearch' onChange={(event) => handleOnChange(event)} placeholder='Введите имя' />
-          </div>
 
-          {/* <Button
-          size={'fit'}
-          onClick={() => {
-            // startGame()
-            // startTimer()
-          }}
-          className='background-gradient-button rounded-full px-6 py-3 text-5xl leading-none text-white transition-colors hover:bg-purple-700'
-          style={{
-            marginBottom: '60px',
-            fontFamily: 'futurespore_cyrillicregular',
-          }}
-        >
-          НАЧАТЬ ИГРУ
-        </Button> */}
-
-          {filteredLeaders && filteredLeaders.length > 0 && (
+          {leaders && leaders.length > 0 ? (
             <>
               <p className='mb-2 text-center font-bold uppercase leading-none text-white'>рейтинг игроков</p>
-              <TableRang leaders={filteredLeaders} />
+              <TableRang leaders={leaders} />
             </>
+          ) : (
+            <div
+              style={{
+                textAlign: 'center',
+                fontFamily: 'futurespore_cyrillicregular',
+                color: 'white',
+                textTransform: 'uppercase',
+              }}
+            >
+              Пусто
+            </div>
           )}
         </div>
       </div>
